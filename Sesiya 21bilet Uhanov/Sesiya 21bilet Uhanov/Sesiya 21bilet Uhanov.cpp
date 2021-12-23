a7812058e5c6cd67cc9949ef15a8f58ad8c702df
caskada <77891927+caskada@users.noreply.github.com>
2021-12-23 17:07:41 +0700

    Add files via upload

---
+++ Sesiya 21bilet Uhanov/Sesiya 21bilet Uhanov/Sesiya 21bilet Uhanov.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    char strok1[1024], strok2[1024];
+    int StrLength = 0;
+    cout << "Введите первую строчку\n";
+    cin >> strok1;
+    cout << "Введите вторую строкчку\n";
+    cin >> strok2;
+    int i = 0;
+    int j = 0;
+    bool key = false;
+    while (strok1[i] != '\0')
+    {
+        while (strok2[j] != '\0')
+        {
+            if (strok1[i] == strok2[j])
+            {
+                key = true;
+                StrLength++;
+                break;
+            }
+            j++;
+        }
+        if (!key)
+        {
+            break;
+        }
+        key = false;
+        i++;
+        j = 0;
+    }
+    cout << StrLength;
+    return 0;
+}
